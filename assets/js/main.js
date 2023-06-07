6ad2968efba8cc28045933a7efff5cad19d94981
muhammamdsurya <muhammadsurya2812@gmail.com>
2023-06-07 18:07:14 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,47 +1,3 @@
-/**
- * Template Name: SoftLand
- * Updated: Mar 10 2023 with Bootstrap v5.2.3
- * Template URL: https://bootstrapmade.com/softland-bootstrap-app-landing-page-template/
- * Author: BootstrapMade.com
- * License: https://bootstrapmade.com/license/
- */
-var swiper = new Swiper(".slide-container", {
-  slidesPerView: 4,
-  spaceBetween: 20,
-  sliderPerGroup: 4,
-  loop: true,
-  centerSlide: true,
-  fade: true,
-  grabCursor: true,
-  autoplay: {
-    delay: 3000, // Mengatur delay antara slide (dalam milidetik)
-    disableOnInteraction: false, // Membuat autoplay tetap berjalan saat pengguna melakukan interaksi dengan swiper
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-    dynamicBullets: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  breakpoints: {
-    0: {
-      slidesPerView: 1,
-    },
-    520: {
-      slidesPerView: 2,
-    },
-    768: {
-      slidesPerView: 3,
-    },
-    1000: {
-      slidesPerView: 4,
-    },
-  },
-});
-
 (function () {
   ("use strict");
 
@@ -173,58 +129,11 @@ var swiper = new Swiper(".slide-container", {
   });
 
   /**
-   * Portfolio details slider
-   */
-  new Swiper(".portfolio-details-slider", {
-    speed: 400,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      type: "bullets",
-      clickable: true,
-    },
-  });
-
-  /**
    * Initiate portfolio details lightbox
    */
   const portfolioDetailsLightbox = GLightbox({
     selector: ".portfolio-details-lightbox",
     width: "90%",
     height: "90vh",
-  });
-
-  /**
-   * Testimonials slider
-   */
-  new Swiper(".testimonials-slider", {
-    speed: 600,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false,
-    },
-    slidesPerView: "auto",
-    pagination: {
-      el: ".swiper-pagination",
-      type: "bullets",
-      clickable: true,
-    },
-  });
-
-  /**
-   * Animation on scroll
-   */
-  window.addEventListener("load", () => {
-    AOS.init({
-      duration: 1000,
-      easing: "ease-in-out",
-      once: true,
-      mirror: false,
-    });
   });
 })();
---
+++ assets/js/vendor.js
@@ -0,0 +1,91 @@
+// Swiper product index
+var swiper = new Swiper(".slide-container", {
+  slidesPerView: 4,
+  spaceBetween: 20,
+  sliderPerGroup: 4,
+  loop: true,
+  centerSlide: true,
+  fade: true,
+  grabCursor: true,
+  autoplay: {
+    delay: 3000, // Mengatur delay antara slide (dalam milidetik)
+    disableOnInteraction: false, // Membuat autoplay tetap berjalan saat pengguna melakukan interaksi dengan swiper
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    520: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    1000: {
+      slidesPerView: 4,
+    },
+  },
+});
+
+/**
+ * Testimonials slider
+ */
+new Swiper(".testimonials-slider", {
+  speed: 600,
+  loop: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  slidesPerView: "auto",
+  pagination: {
+    el: ".swiper-pagination",
+    type: "bullets",
+    clickable: true,
+  },
+});
+
+/**
+ * Portfolio details slider
+ */
+new Swiper(".portfolio-details-slider", {
+  speed: 400,
+  loop: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    type: "bullets",
+    clickable: true,
+  },
+});
+
+//parallax
+var image = document.getElementsByClassName("img-thumbnail");
+new simpleParallax(image);
+
+// Pure counter
+new PureCounter();
+
+/**
+ * Animation on scroll
+ */
+window.addEventListener("load", () => {
+  AOS.init({
+    duration: 1000,
+    easing: "ease-in-out",
+    once: true,
+    mirror: false,
+  });
+});
